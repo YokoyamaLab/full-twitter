f4c0bf0da0e4c3387a628d91f0164912c8a3907c
Shohei Yokoyama <1260486+abarth500@users.noreply.github.com>
2021-06-05 20:27:37 +0900

    npx

--- bin.mjs
+++ bin.mjs
@@ -4,18 +4,19 @@ const program = new Command();
 
 program
     .name("node bin.mjs")
-    .option('-q, --query <path>', 'クエリJSONファイル','./query.json')
-    .option('-i, --input <path>','Twiiterアーカイブディレクトリ名','/data/local/twitter')
-    .option('-o, --output <path>', '結果出力ディレクトリ名','result')
+    .requiredOption('-q, --query <path>', 'クエリJSONファイル')
+    .option('-i, --input <path>', 'Twiiterアーカイブディレクトリ名', '/data/local/twitter')
+    .option('-o, --output <path>', '結果出力ディレクトリ名', 'result')
     .option('-m, --outputMaxLines <integer>', '結果ファイル一つ当たりの行数', 1000000)
     .option('-p, --n-parallel <integer>', '並列実行スレッド数', 45)
     .option('-c, --n-concurrence <integer>', 'スレッド毎の平行処理ファイル数', 4)
     .option('-n, --prevent-resume', 'レジュームをせず新しいセッションを実行する')
     .option('-r, --resume <path>', 'レジューム用作業記録ファイル', '.resume')
-    .option('-v, --verbose','途中作成ファイルを全て残す')
+    .option('-v, --verbose', '途中作成ファイルを全て残す')
     .option('-f, --day-from <yyyy-mm-dd>', '処理対象期間(開始)ISO8601形式', '2020-04-01T00:00:00')
     .option('-t, --day-to <yyyy-mm-dd>', '処理対象期間(終了)ISO8601形式', '2021-03-31T23:59:59')
-    .option('-h, --hour-window <[h,h,...]>', '収集対象時間帯');
+    .option('-h, --hour-window <[h,h,...]>', '収集対象時間帯')
+    .helpOption('-?, --help', 'コマンドの使用方法表示');
 
 program.parse(process.argv);
 let config = program.opts();
